9ab4937f59abaa991f152ccede20e2c07876484e
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-01 13:07:58 -0600

    Create new test to check for Braille translation

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,12 @@
 require './lib/file_reader'
+require './lib/translator'
+
 class NightWriter
   attr_reader :reader, :output_file, :input_file
 
   def initialize
+    @braille = Translator.new
+    @braille.dictionary
     @reader = FileReader.new
     @input_file = ARGV[0]
     @output_file = ARGV[1]
@@ -19,13 +23,17 @@ class NightWriter
     # braille = encode_to_braille(plain)
   end
 
+  def change_to_braille
+  end
+
   def encode_to_braille
     message = @reader.read('message.txt').chomp
+    encoded_message = @braille.encode(message)
     File.open(@output_file, 'w+') do |file|
-      file.write message
+      file.write encoded_message
       file.close
     end
-    # @reader.read(@input_file)
+    @reader.read(@input_file)
   end
 
   def new_file_with_braille
@@ -35,6 +43,6 @@ end
 # puts ARGV.inspect
 # ARGV.replace
 
-# night_writer = NightWriter.new
+night_writer = NightWriter.new
 # night_writer.created
-# night_writer.encode_to_braille
+night_writer.encode_to_braille
--- lib/translator.rb
+++ lib/translator.rb
@@ -6,35 +6,46 @@ class Translator
   end
 
   def dictionary
-    @encoding[' '] = ["..", "..", ".."]
-    @encoding['a'] = ["0.", "..", ".."]
-    @encoding['b'] = ["0.", "0.", ".."]
-    @encoding['c'] = ["..", "00", ".."]
-    @encoding['d'] = ["00", ".0", ".."]
-    @encoding['e'] = ["0.", ".0", ".."]
-    @encoding['f'] = ["00", "0.", ".."]
-    @encoding['g'] = ["00", "00", ".."]
-    @encoding['h'] = ["0.", "00", ".."]
-    @encoding['i'] = [".0", "0.", ".."]
-    @encoding['j'] = [".0", "00", ".."]
-    @encoding['k'] = ["0.", "..", "0."]
-    @encoding['l'] = ["0.", "0.", "0."]
-    @encoding['m'] = ["00", "..", "0."]
-    @encoding['n'] = ["00", ".0", "0."]
-    @encoding['o'] = ["0.", ".0", "0."]
-    @encoding['p'] = ["00", "0.", "0."]
-    @encoding['q'] = ["00", "00", "0."]
-    @encoding['r'] = ["0.", "00", "0."]
-    @encoding['s'] = [".0", "0.", "0."]
-    @encoding['t'] = [".0", "00", "0."]
-    @encoding['u'] = ["0.", "..", "00"]
-    @encoding['v'] = ["0.", "0.", "00"]
-    @encoding['w'] = [".0", "00", ".0"]
-    @encoding['x'] = ["00", "..", "00"]
-    @encoding['y'] = ["00", ".0", "00"]
-    @encoding['z'] = ["0.", ".0", "00"]
+    @encoding[' '] = [". .", ". .", ". ."]
+    @encoding['a'] = ["0 .", ". .", ". ."]
+    @encoding['b'] = ["0 .", "0 .", ". ."]
+    @encoding['c'] = [". .", "0 0", ". ."]
+    @encoding['d'] = ["0 0", ". 0", ". ."]
+    @encoding['e'] = ["0 .", ". 0", ". ."]
+    @encoding['f'] = ["0 0", "0 .", ". ."]
+    @encoding['g'] = ["0 0", "0 0", ". ."]
+    @encoding['h'] = ["0 .", "0 0", ". ."]
+    @encoding['i'] = [". 0", "0 .", ". ."]
+    @encoding['j'] = [". 0", "0 0", ". ."]
+    @encoding['k'] = ["0 .", ". .", "0 ."]
+    @encoding['l'] = ["0 .", "0 .", "0 ."]
+    @encoding['m'] = ["0 0", ". .", "0 ."]
+    @encoding['n'] = ["0 0", ". 0", "0 ."]
+    @encoding['o'] = ["0 .", ". 0", "0 ."]
+    @encoding['p'] = ["0 0", "0 .", "0 ."]
+    @encoding['q'] = ["0 0", "0 0", "0 ."]
+    @encoding['r'] = ["0 .", "0 0", "0 ."]
+    @encoding['s'] = [". 0", "0 .", "0 ."]
+    @encoding['t'] = [". 0", "0 0", "0 ."]
+    @encoding['u'] = ["0 .", ". .", "0 0"]
+    @encoding['v'] = ["0 .", "0 .", "0 0"]
+    @encoding['w'] = [". 0", "0 0", ". 0"]
+    @encoding['x'] = ["0 0", ". .", "0 0"]
+    @encoding['y'] = ["0 0", ". 0", "0 0"]
+    @encoding['z'] = ["0 .", ". 0", "0 0"]
     @encoding
   end
 
-
+  def encode(message)
+    first_row = message.chars.map do |letter|
+       "#{@encoding[letter][0]} "
+    end
+    second_row = message.chars.map do |letter|
+       "#{@encoding[letter][1]} "
+    end
+    third_row = message.chars.map do |letter|
+     "#{@encoding[letter][2]} "
+    end
+    "#{first_row.join}\n#{second_row.join}\n#{third_row.join}"
+  end
 end
